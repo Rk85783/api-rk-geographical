2f337167466879182992dbac3fc00df548990706
Rohit Kumar Mahor (rk85783) <rk85783@gmail.com>
2025-03-10 10:00:10 +0530

    deprecate - truck-types api

--- app.js
+++ app.js
@@ -167,32 +167,5 @@ app.get("/api/carriers", async (req, res) => {
   }
 });
 
-// -----> Carriers
-app.get("/api/truck-types", async (req, res) => {
-  try {
-    const { page = 1, limit = 100, sort = 'id', order = 'asc', ...filters } = req.query;
-    const sortOrder = order === 'asc' ? 1 : -1;
-
-    const filterConditions = {};
-    for (const [key, value] of Object.entries(filters)) {
-      filterConditions[key] = new RegExp(value, 'i');
-    }
-
-    const [totalCount, data] = await Promise.all([
-      mongoose.connection.db.collection("truckTypes").countDocuments(filterConditions),
-      mongoose.connection.db.collection("truckTypes").find(filterConditions)
-        .sort({ [sort]: sortOrder })
-        .skip((page - 1) * limit)
-        .limit(parseInt(limit))
-        .toArray()
-    ]);
-
-    const totalPages = Math.ceil(totalCount / limit);
-    res.status(200).json({ totalPages, totalCount, currentPage: parseInt(page), data });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`App is running at http://localhost:${PORT}`));
